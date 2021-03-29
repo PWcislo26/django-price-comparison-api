630613364ed1449c11e96c53c74c92296f809c00
Piotr <piotrwcislo26@gmail.com>
2021-03-29 15:39:57 +0200

    Function optimization

--- product/tasks.py
+++ product/tasks.py
@@ -8,25 +8,31 @@ django.setup()
 
 import requests
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 from core.models import Product
 from decimal import Decimal
 from product.serializers import ProductSerializer
+import time
 
 
 def get_price_morele():
     """Get product prices from morele via link and return prices list"""
     products_list = Product.objects.all()
     product_prices = []
+    request_session = requests.Session()
     for product in products_list:
-        try:
-            page = requests.get(product.link_morele, headers={'User-Agent': 'Mozilla/5.0'})
-            content = BeautifulSoup(page.content, 'html.parser')
-            price = Decimal(content.find('div', id='product_price_brutto').text.replace(' ', '').strip()[:-2])
-            product_prices.append(price)
+        if product.link_morele:
+            try:
+                page = request_session.get(product.link_morele, headers={'User-Agent': 'Mozilla/5.0'})
+                content = BeautifulSoup(page.content, 'lxml', parse_only=SoupStrainer('div', id='product_price_brutto'))
+                price = Decimal(content.text.strip().replace(' ', '').replace(',', '.')[:-2])
+                product_prices.append(price)
 
-        except Exception as e:
-            print("scraping failed, exception is: ")
-            print(e)
+            except Exception as e:
+                print("scraping failed, exception is: ")
+                print(e)
+        else:
+            product_prices.append('')
     return product_prices
 
 
@@ -34,16 +40,19 @@ def get_price_xkom():
     """Get product prices from xkom via link and return prices list"""
     products_list = Product.objects.all()
     product_prices = []
+    request_session = requests.Session()
     for product in products_list:
-        try:
-            page = requests.get(product.link_xkom, headers={'User-Agent': 'Opera/9.60'})
-            content = BeautifulSoup(page.content, 'html.parser')
-            price = Decimal(content.find('div', class_='u7xnnm-4 jFbqvs').text.replace(' ', '').replace(',', '.')[:-2])
-            product_prices.append(price)
-
-        except Exception as e:
-            print("scraping failed, exception is: ")
-            print(e)
+        if product.link_xkom:
+            try:
+                page = request_session.get(product.link_xkom, headers={'User-Agent': 'Opera/9.60'})
+                content = BeautifulSoup(page.content, 'lxml', parse_only=SoupStrainer('div', class_='u7xnnm-4 jFbqvs'))
+                price = Decimal(content.text.replace(' ', '').replace(',', '.')[:-2])
+                product_prices.append(price)
+            except Exception as e:
+                print("scraping failed, exception is: ")
+                print(e)
+        else:
+            product_prices.append('')
     return product_prices
 
 
@@ -51,18 +60,19 @@ def get_price_proline():
     """Get product prices from proline via link and return prices list"""
     products_list = Product.objects.all()
     product_prices = []
+    request_session = requests.Session()
     for product in products_list:
-        try:
-            page = requests.get(product.link_proline, headers={'User-Agent': 'Opera/9.60'})
-            content = BeautifulSoup(page.content, 'html.parser')
-            price = Decimal(
-                content.find('table', class_='cenaline_karta').find('b', class_='cena_b').text.replace(' ', '').replace(
-                    ',', '.')[:-3])
-            product_prices.append(price)
-
-        except Exception as e:
-            print("scraping failed, exception is: ")
-            print(e)
+        if product.link_proline:
+            try:
+                page = request_session.get(product.link_proline, headers={'User-Agent': 'Opera/9.60'})
+                content = BeautifulSoup(page.content, 'lxml', parse_only=SoupStrainer('table', class_='cenaline_karta'))
+                price = Decimal(content.text[51:].split('zł')[0].strip().replace(',', '.'))
+                product_prices.append(price)
+            except Exception as e:
+                print("scraping failed, exception is: ")
+                print(e)
+        else:
+            product_prices.append('')
     return product_prices
 
 
@@ -82,4 +92,7 @@ def update_prices():
             serializer.save()
 
 
+start = time.time()
 update_prices()
+end = time.time()
+print(end-start)
