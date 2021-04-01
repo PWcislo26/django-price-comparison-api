3936aee3ad0927d2ae093cc4c4a2a6c3cab6deab
Piotr <piotrwcislo26@gmail.com>
2021-04-01 14:29:20 +0200

    Updated user views, serializers, urls

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from users import views
 
+app_name ='users'
 
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view()),
-    path('token/', views.CreateTokenView.as_view()),
-    path('me/', views.ManageUserView.as_view()),
+    path('register/', views.UserCreate.as_view(), name="create_user"),
+    path('logout/blacklist', views.BlacklistTokenView.as_view(), name='blacklist')
 ]
--- users/views.py
+++ users/views.py
@@ -1,27 +1,31 @@
-from rest_framework import generics, authentication, permissions
-from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import generics, authentication, permissions, status
 from rest_framework.settings import api_settings
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
-from users.serializers import UserSerializer, AuthTokenSerializer
+from .serializers import RegisterUserSerializer
+
+class UserCreate(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        reg_serializer = RegisterUserSerializer(data = request.data)
+        if reg_serializer.is_valid():
+            user = reg_serializer.save()
+            if user:
+                return Response(status=status.HTTP_201_CREATED)
+        return Response(reg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CreateUserView(generics.CreateAPIView):
-    """Create a new user in the system"""
-    serializer_class = UserSerializer
+class BlacklistTokenView(APIView):
+    permission_classes = [AllowAny]
 
-
-class CreateTokenView(ObtainAuthToken):
-    """Create a new auth token for user"""
-    serializer_class = AuthTokenSerializer
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-
-
-class ManageUserView(generics.RetrieveUpdateAPIView):
-    """Manage authenticated user"""
-    serializer_class = UserSerializer
-    authentication_classes = (authentication.TokenAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def get_object(self):
-        """retrieve and return authenticated user"""
-        return self.request.user
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
